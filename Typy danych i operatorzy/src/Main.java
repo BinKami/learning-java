5e1a544e59a76d012bc26a75fab62fd6c49b427f
BinKami <kakoro@op.pl>
2025-10-14 23:13:43 +0200

    Add files via upload

---
+++ Typy danych i operatorzy/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        double d = 10.0;
+        double e = 5.0;
+
+
+        int c = a + b; // całkowite
+        double f = e / d; // double zmienno przecinkowe
+
+        // % "modulo" daje nam reszę z dzielenia 
+
+        System.out.println(a); // sout
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(f);
+    }
+}
