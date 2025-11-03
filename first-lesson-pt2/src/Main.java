4cd6adff1ec66ef79ba6ccd3b908b30be2698cef
BinKami <kakoro@op.pl>
2025-11-03 23:12:20 +0100

    Add files via upload

---
+++ first-lesson-pt2/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj swoje imię: ");
+        String firstname = scanner.nextLine();
+
+        System.out.print("Podaj swoje nazwisko: ");
+        String lastname = scanner.nextLine();
+
+        System.out.print("Podaj swój wiek: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Witaj " + firstname + " " + lastname + ". Masz " + age + " lat." );
+    }
+}
